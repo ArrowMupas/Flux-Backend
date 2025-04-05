3d7c6538d26b53a0a7a015d06f06aa1b7859ea72
Arrow <delmontearrow2004@gmai;.com>
2025-04-05 20:11:12 +0800

    backend foundation can work with frontend

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 require("dotenv").config();
 const productRoute = require("./routes/productRoute");
+const errorMiddleware = require("./middlewares/errorMiddleware");
+const cors = require("cors");
 
 const app = express();
+
+const FRONTEND = process.env.FRONTEND;
+const corOptions = {
+  origin: FRONTEND,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use("/api", productRoute);
+app.use("/api/products", productRoute);
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`Node API is running on port ${process.env.PORT}`);
