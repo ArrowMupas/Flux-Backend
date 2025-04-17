2eafd3877f5e005bdde3e654474f2b5988384f59
ArrowMupas <delmontearrow2004@gmail.com>
2025-04-17 14:57:27 +0800

    This better work Clark

--- database/init.sql
+++ database/init.sql
@@ -30,7 +30,8 @@ CREATE TABLE IF NOT EXISTS user (
     contact_number VARCHAR(20),         
     role_id INT DEFAULT 2,                      
     email VARCHAR(100) UNIQUE NOT NULL, 
-    password VARCHAR(255) NOT NULL,   
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    password_hash VARCHAR(255) NOT NULL,   
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (role_id) REFERENCES roles(id)
 );
--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,10 +1,12 @@
 const errorMiddleware = (err, req, res, next) => {
-  console.log("Error: ", err);
-  const statusCode = res.statusCode ? res.statusCode : 500;
-  res.status(statusCode);
-  res.json({
+  const statusCode =
+    res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+  console.error(`Error: ${err.message}`);
+  res.status(statusCode).json({
+    success: false,
+    status: statusCode,
     message: err.message,
-    stack: process.env.NODE_ENV === "development" ? err.stack : null,
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
 };
 
