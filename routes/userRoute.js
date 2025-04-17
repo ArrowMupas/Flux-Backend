70e8cc6ef8530b5995db4341c30ab927fc09f91d
ArrowMupas <delmontearrow2004@gmail.com>
2025-04-17 15:50:49 +0800

    fixed code, download prettier for formatting

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/userController");
+const userController = require('../controllers/userController');
 
-
-router.post("/register", userController.registerUser);
-router.post("/login", userController.loginUser);
-router.get("/:id", userController.getUserProfile);
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.get('/:id', userController.getUserProfile);
 router.put('/:id', userController.updateUser);
 
 module.exports = router;
