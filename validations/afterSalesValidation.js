fd42ebe0bb9d907665a84261e666062488cf41dd
ArrowMupas <delmontearrow2004@gmail.com>
2025-05-29 18:33:08 +0800

    letting the unknown

--- validations/afterSalesValidation.js
+++ validations/afterSalesValidation.js
@@ -4,7 +4,7 @@ const validate = require('../helpers/validateHelper');
 const requestSchema = Joi.object({
     order_id: Joi.string().max(50).required(),
     reason: Joi.string().max(255).required(),
-});
+}).unknown();
 
 const validateRequest = validate(requestSchema);
 
--- validations/walkInOrderValidation.js
+++ validations/walkInOrderValidation.js
@@ -4,7 +4,7 @@ const validate = require('../helpers/validateHelper');
 const itemSchema = Joi.object({
     product_id: Joi.string().max(10).required(),
     quantity: Joi.number().integer().min(1).required(),
-});
+}).unknown();
 
 const WalkInOderSchema = Joi.object({
     customer_name: Joi.string().max(100).required(),
@@ -12,7 +12,7 @@ const WalkInOderSchema = Joi.object({
     notes: Joi.string().max(255).optional().allow(''),
     discount_amount: Joi.number().min(0).precision(2).required(),
     items: Joi.array().items(itemSchema).min(1).required(),
-});
+}).unknown();
 
 const validateWalkInOrder = validate(WalkInOderSchema);
 
