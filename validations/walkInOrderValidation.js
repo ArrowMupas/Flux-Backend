919dff40059a15ae17fb1343e48b502067a1e092
Arrow <delmontearrow2004@gmai;.com>
2025-06-15 22:52:25 +0800

    BackEnd Thesis Demo Version

--- validations/walkInOrderValidation.js
+++ validations/walkInOrderValidation.js
@@ -8,7 +8,7 @@ const itemSchema = Joi.object({
 
 const WalkInOderSchema = Joi.object({
     customer_name: Joi.string().max(100).required(),
-    customer_email: Joi.string().email().max(100).required(),
+    customer_email: Joi.string().email().max(100).optional(),
     notes: Joi.string().max(255).optional().allow(''),
     discount_amount: Joi.number().min(0).precision(2).required(),
     items: Joi.array().items(itemSchema).min(1).required(),
